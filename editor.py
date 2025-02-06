d352456a5157bb1ac3132b1028b99fc6813689aa
tosu! <theo.constant42@gmail.com>
2025-02-06 20:24:24 +0100

    Brush size

--- editor.py
+++ editor.py
@@ -5,13 +5,13 @@ from scripts.core.utils import load_images
 from scripts.core.tilemap import TileMap
 from scripts.core.utils import MAP_PATH
 
-EDITOR_SCREEN_SIZE = (640, 480)
+EDITOR_SCREEN_SIZE = (640*2, 480*2)
 RENDER_SCALE = 1
 
 class Editor:
     # ===== SINGLETON =====
     __instance = None
-    def __new__(cls, name):
+    def __new__(cls, name=None):
         if cls.__instance is None:
             cls.__instance = super(Editor, cls).__new__(cls)
             cls.__instance.__initialized = False
@@ -39,6 +39,7 @@ class Editor:
 
         self.tilemap = TileMap(self)
         if name:
+            self.name = name
             self.tilemap.load(MAP_PATH + '/' + name + '/map.json')
 
         self.scroll          = [0, 0]
@@ -50,8 +51,10 @@ class Editor:
         self.right_clicking  = False
         self.shift_clicking  = False
         self.ctrl_clicking   = False
+        self.alt_clicking   = False
         self.start_mouse_pos = (0, 0)
         self.start_scroll    = (0, 0)
+        self.brush_size = 1
 
     def run(self):
         while True:
@@ -70,11 +73,19 @@ class Editor:
 
             if self.left_clicking and not self.shift_clicking:
                 if self.on_grid:
-                    self.tilemap.tilemap[str(tile_pos[0])+';'+str(tile_pos[1])] = {'type': self.tile_list[self.tile_group], 'variant': self.tile_variant, 'pos': tile_pos}
+                    for i in range(-(self.brush_size // 2), (self.brush_size // 2) + self.brush_size % 2):
+                        for j in range(-(self.brush_size // 2), (self.brush_size // 2) + self.brush_size % 2):
+                            self.tilemap.tilemap[str(tile_pos[0] + i) + ';' + str(tile_pos[1] + j)] = {
+                                'type': self.tile_list[self.tile_group],
+                                'variant': self.tile_variant,
+                                'pos': [tile_pos[0] + i, tile_pos[1] + j]
+                            }
             if self.right_clicking:
-                tile_loc = str(tile_pos[0])+';'+str(tile_pos[1])
-                if tile_loc in self.tilemap.tilemap:
-                    del self.tilemap.tilemap[tile_loc]
+                for i in range(-(self.brush_size // 2), (self.brush_size // 2) + self.brush_size % 2):
+                    for j in range(-(self.brush_size // 2), (self.brush_size // 2) + self.brush_size % 2):
+                        tile_loc = str(tile_pos[0] + i) + ';' + str(tile_pos[1] + j)
+                        if tile_loc in self.tilemap.tilemap:
+                            del self.tilemap.tilemap[tile_loc]
                 for tile in self.tilemap.offgrid_tiles.copy():
                     tile_img = self.assets[tile['type']][tile['variant']]
                     tile_rect = pygame.Rect(tile['pos'][0] - self.scroll[0], tile['pos'][1] - self.scroll[1], tile_img.get_width(), tile_img.get_height()) 
@@ -115,11 +126,16 @@ class Editor:
                 if event.type == pygame.MOUSEWHEEL:
                     if self.shift_clicking:
                         self.tile_variant = (self.tile_variant + event.y) % len(self.assets[self.tile_list[self.tile_group]])
+                    elif self.alt_clicking:
+                        self.brush_size += event.y
+                        self.brush_size = max(0, self.brush_size)
+                        print(f'Brush size: {self.brush_size}')
                     else:
                         self.tile_group = (self.tile_group + event.y) % len(self.tile_list)
                         self.tile_variant = 0
 
                 if event.type == pygame.KEYDOWN:
+                    # Movement
                     if event.key == pygame.K_LEFT:
                         self.movement[0] = True
                     if event.key == pygame.K_RIGHT:
@@ -128,21 +144,30 @@ class Editor:
                         self.movement[2] = True
                     if event.key == pygame.K_DOWN:
                         self.movement[3] = True
+                    # Special keys
                     if event.key == pygame.K_LSHIFT:
                         self.shift_clicking = True
                     if event.key == pygame.K_LCTRL:
                         self.ctrl_clicking = True
+                    if event.key == pygame.K_LALT:
+                        self.alt_clicking = True
+                    # Grid
                     if event.key == pygame.K_g:
                         self.on_grid = not self.on_grid
                         print(f"{'grid' if self.on_grid else 'not grid'}")
+                    # Auto tile
                     if event.key == pygame.K_t:
-                        print('autotile')
                         self.tilemap.autotile()
+                        print('autotile')
+                    # Saving
                     if event.key == pygame.K_s:
                         if self.ctrl_clicking:
-                            print('saving...')
-                            self.tilemap.save('data/maps/map.json')
-                            print('saved')
+                            if self.name:
+                                self.tilemap.save(MAP_PATH + '/' + self.name + '/map.json')
+                                print(f'Saved in {MAP_PATH + '/' + self.name + '/map.json'}')
+                            else:
+                                self.tilemap.save('data/maps/map.json')
+                                print('Saved in data/maps/map.json')
 
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
@@ -157,7 +182,8 @@ class Editor:
                         self.shift_clicking = False
                     if event.key == pygame.K_LCTRL:
                         self.ctrl_clicking = False
-
+                    if event.key == pygame.K_LALT:
+                        self.alt_clicking = False
 
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
@@ -165,4 +191,4 @@ class Editor:
 
 
 # ========================================
-Editor('map1').run()
+Editor('Kayou').run()
